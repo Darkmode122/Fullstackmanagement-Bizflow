d71b1829b1095da08b751b0d65ba27b57e5bcba1
Darkmode122 <170638883+Darkmode122@users.noreply.github.com>
2026-03-08 17:09:20 +0800

    Changes made fixed app.js

--- Bizflowapp-Backend/app.js
+++ Bizflowapp-Backend/app.js
@@ -1,8 +1,21 @@
-const express  = require(express)
-const app = express
+// imports the Express.js framework
+const express = require('express');
+// Creates an instance of the Express application. This app variable will be used to define routes and configure the server.
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+
+// Middleware to enable CORS (Cross-Origin Resource Sharing)
+app.use(cors());
+const port = 3000;
+
+// Defines a route for the root URL (/). When a client makes a GET request to the root URL,
+// the server responds with the message "Hello World!"
+app.get('/', (req, res) => {
+  return res.json("From backend side");
+});
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-me.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`listening...`);
 });
