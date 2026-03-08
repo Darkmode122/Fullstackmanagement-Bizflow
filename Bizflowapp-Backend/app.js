5f844d0618961090f1f1abdd1731f7ce18a3d771
Darkmode122 <170638883+Darkmode122@users.noreply.github.com>
2026-03-08 13:31:42 +0800

    Changes adjustments made

---
+++ Bizflowapp-Backend/app.js
@@ -0,0 +1,8 @@
+const express  = require(express)
+const app = express
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+me.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
