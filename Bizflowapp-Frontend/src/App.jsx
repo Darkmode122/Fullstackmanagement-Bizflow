deee7dfa15ade002f51bb87ca6decdabc9462a28
Darkmode122 <170638883+Darkmode122@users.noreply.github.com>
2026-03-09 21:44:23 +0800

    Changes made by me

--- Bizflowapp-Frontend/src/App.jsx
+++ Bizflowapp-Frontend/src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </div>
   );
 }
--- Bizflowapp-Frontend/src/main.jsx
+++ Bizflowapp-Frontend/src/main.jsx
@@ -1,6 +1,7 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import './App.css';
 
-createRoot(document.getElementById('sandy')).render(
-  <p>Welcome!</p>
-  
-) 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
