933edd2fb6f051cd264d1c01c9d5a763ebff2a5d
Darkmode122 <170638883+Darkmode122@users.noreply.github.com>
2026-03-11 14:21:09 +0800

    Changes made by me

--- Bizflowapp-Frontend/src/App.jsx
+++ Bizflowapp-Frontend/src/App.jsx
@@ -1,16 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
-    <div>
-      <ul>
-        <li className="logo"><img src="logo.png" alt="Logo" /></li>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li className="navbarleft"><a href="#about">About Us</a></li>
-        <li><a href="#signin">Sign In</a></li>
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <Link className='linklogo' to="/home">
+              <img src="\logo.png" alt="Logo"/>
+            </Link>
+          </li>
+
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+
+        <li className="navbarleft">
+          <Link to="/about">About Us</Link>
+        </li>
+
+        <li><Link to="/signin">Sign In</Link></li>
       </ul>
-      
     </div>
+    </nav>
   );
 }
 
