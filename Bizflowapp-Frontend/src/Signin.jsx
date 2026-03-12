5580920ce20fc95eaeea683ab0764dd12e638ca0
Darkmode122 <170638883+Darkmode122@users.noreply.github.com>
2026-03-12 23:21:14 +0800

    Changes made in signup page

--- Bizflowapp-Frontend/src/Signin.jsx
+++ Bizflowapp-Frontend/src/Signin.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 function Signin() {
+  const [name, setName] = useState('');
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(name);
+  };
+
   return (
-    <form>
-      <label>Enter your name:
-        <input type="text" />
+     <form onSubmit={handleSubmit}>
+      <label>Email:
+        <input
+          type="text" 
+          value={name}
+          onChange={handleChange}
+        />
       </label>
+      <label>Password:
+        <input
+          type="password" 
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" />
     </form>
   )
 }
